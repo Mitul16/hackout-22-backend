c511c3bb4790c658150edf4a50b29d74e0ee4320
Mitul16 <Mitul16@users.noreply.github.com>
2022-10-14 20:34:57 +0530

    chore: Minor cleanup

--- app/server.js
+++ app/server.js
@@ -1,28 +1,41 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
+
+// API routers
 const authRoutes = require('./routes/authRoutes')
 const exampleRoutes = require('./routes/exampleRoute')
+
+// API middlewares
 const { notFound, errorHandler } = require('./middlewares/errorMiddleware')
 
-const app = express()
+// Update environment variables via `.env` file
 dotenv.config()
+
+// Create `express` server
+const app = express()
+
+// Connect to MongoDB server
 connectDB()
 
+// Add the data parsers
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Attach API routes / routers
 app.get('/', (req, res) => res.send('API is Running'))
 app.use('/api/auth', authRoutes)
 app.use('/api/example', exampleRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
+// Start listening for client requests
 const port = process.env.PORT || 3000
 const server = app.listen(port, () =>
   console.log(`Sever running on port ${port}`),
 )
 
+// For any other unhandled error(s)
 process.on('unhandledRejection', (err, promise) => {
   console.log(`Logged Error: ${err.message}`)
   server.close(() => process.exit(1))
