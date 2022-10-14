da51fa8ea898b1f7bd42d22753fcfb6c2e54645b
pankaj1707k <pankaj.1707k@gmail.com>
2022-10-15 00:33:37 +0530

    feat: Add api route for remove project

--- app/routes/projectRoutes.js
+++ app/routes/projectRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const { removeProject } = require('../controllers/projectControllers')
 const router = express.Router()
 
 
 router.route("/add").post(protected , addUser)
+router.route("/projects/:id").delete(protected, removeProject)
