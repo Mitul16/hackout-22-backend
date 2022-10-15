2481cebd4ae4ed8b5ab8b6b80164743fdf2141b8
pankaj1707k <pankaj.1707k@gmail.com>
2022-10-15 13:28:04 +0530

    refactor: remove exampleController and exampleRoute (not required anymore)

--- app/controllers/exampleController.js
+++
@@ -1,8 +0,0 @@
-const asyncHandler = require('express-async-handler')
-const ErrorResponse = require('../utils/errorResponse')
-
-const TestExample = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ success: true, data: 'Access to data granted' })
-})
-
-module.exports = { TestExample }
--- app/routes/exampleRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { TestExample } = require('../controllers/exampleController')
-const { protected } = require('../middlewares/authMiddleware')
-const router = express.Router()
-
-router.route('/').get(protected, TestExample)
-
-module.exports = router
